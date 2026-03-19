6038eabe0b05bd62ac568c386f9a00f3fc4874d2
Samuel <samuelstonecaballero@gmail.com>
2026-03-19 19:59:26 +0100

    Organizado backend y tabla usuario

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, DateTime ,ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from database import Base
 
 class Usuario(Base):
@@ -7,6 +9,12 @@ class Usuario(Base):
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String(100))
     contrasena = Column(String(255))
-    correo = Column(String(100))
-    fecha_registro = Column(DateTime, default=datetime.utcnow)
+    correo_electronico = Column(String(100),index=True, unique=True)
+    fecha_registro = Column(DateTime(), server_default=func.now())
     telefono = Column(String(20))
+
+    # relacion con tabla
+    notificacion = relationship("Notificacion", back_populates="usuario")
+    usuario_trofeo = relationship("Usuario_Trofeo", back_populates="usuario")
+    suscripcion = relationship("Suscripcion", back_populates="usuario")
+    cuenta_trading = relationship("Cuenta_Trading", back_populates="usuario")
